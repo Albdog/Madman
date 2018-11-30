b706a226baa7f1374db6af7ae3aa2ff4062b5e6a
Albdog <joaquinjacinto11@gmail.com>
2018-12-01 00:38:36 +0800

    BeeMovement - added logic for surrounding player

--- Assets/Scipts/BeeMovement.cs
+++ Assets/Scipts/BeeMovement.cs
@@ -8,6 +8,7 @@ public class BeeMovement : MonoBehaviour {
     public float followSpeed;
     private Transform target;
     private float buzzSpeed = 5;
+    private bool isFollowing = true;
     private Vector3 speedRot = Vector3.right * 50f;
 
     void Start() {
@@ -15,28 +16,37 @@ public class BeeMovement : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update() {
         transform.LookAt(target);
+        float targetY = target.position.y + 0.8f;
 
-        if(followSpeed != 0) {
-            float targetY = target.position.y + 0.8f;
+        if(isFollowing) {
             Vector3 head = new Vector3(target.position.x, targetY, target.position.z);
 
             transform.position = Vector3.MoveTowards(transform.position, head, followSpeed * Time.deltaTime);
-        } else {
-
         }
-	}
+        else {
+            float x = Random.Range(target.position.x - 50f, target.position.x + 50f);
+            float y = Random.Range(targetY - 15f, targetY + 15f);
+            float z = Random.Range(target.position.z - 50f, target.position.z + 50f);
+
+            Vector3 flyTo = new Vector3(x, y, z);
+
+            transform.position = Vector3.MoveTowards(transform.position, flyTo, buzzSpeed * Time.deltaTime);
+        }
+    }
 
     private void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")) {
             followSpeed = 0;
+            isFollowing = false;
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if(other.CompareTag("Player")) {
             followSpeed = 5;
+            isFollowing = true;
         }
     }
 }
