03130f0414e6280e9126fbdad443d475146f2ff2
Albdog <joaquinjacinto11@gmail.com>
2018-12-02 02:53:34 +0800

    SchizoBarManager - added logic for manipulating schizo bar

---
+++ Assets/Scipts/SchizoBarManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SchizoBarManager : MonoBehaviour {
+    
+    public Slider schizoFill;
+    public float currentSchizoLevel;
+    public float maxSchizoLevel;
+
+	public void ChangeSchizoLevel(int amount) {
+        currentSchizoLevel += amount;
+        currentSchizoLevel = Mathf.Clamp(currentSchizoLevel, 0, maxSchizoLevel);
+
+        schizoFill.value = currentSchizoLevel / maxSchizoLevel;
+    }
+}
