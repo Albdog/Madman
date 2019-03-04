ed3c04b2d188036858764ff9c7f33b4f97b7dac2
kurtdeleon <kurt.deleon@obf.ateneo.edu>
2019-03-04 21:40:24 +0800

    Grass fixes and repositioning of bldg
    Also added invisible walls near the gate. Shadow script is still incomplete

--- Assets/Scripts/ShadowPositioner.cs
+++ Assets/Scripts/ShadowPositioner.cs
@@ -2,19 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShadowPositioner : MonoBehaviour {
+public class ShadowPositioner : MonoBehaviour
+{
 
     private GameObject player;
     private bool isColliding;
     [SerializeField] public float updateFrequency;
     [SerializeField] public float teleportRange;
     [SerializeField] public float protectiveRange;
+    private float groundLevelHeight;
 
-	// Use this for initialization
-	void Awake () {
+    // Use this for initialization
+    void Awake()
+    {
         player = GameObject.FindGameObjectWithTag("Player");
         isColliding = false;
         StartCoroutine(UpdatePosition());
+        groundLevelHeight = 1.8369f; //y-position if person is on ground level
     }
 
     // Update is called once per frame
@@ -28,13 +32,13 @@ public class ShadowPositioner : MonoBehaviour {
         while (true)
         {
             //update its position
-            do {
+            do
+            {
                 SetNewPosition();
             } while (isColliding);
-               
+
             //TODO
             //Obscure shadow from view before moving
-            //Check if player is on upper floors
 
             //wait for updateFrequency seconds
             yield return new WaitForSeconds(3.0f);
@@ -47,18 +51,32 @@ public class ShadowPositioner : MonoBehaviour {
         Vector3 playerPos = player.transform.position;
 
         //set shadow to be same y as player
-        this.gameObject.transform.position = new Vector3(
+        if (playerPos.y <= groundLevelHeight) //checks if player is on ground level
+        {
+            this.gameObject.transform.position = new Vector3(
             playerPos.x + Random.Range(protectiveRange, teleportRange) * ((Random.Range(0, 2) == 0) ? 1 : -1),
             playerPos.y,
             playerPos.z + Random.Range(protectiveRange, teleportRange) * ((Random.Range(0, 2) == 0) ? 1 : -1)
             );
+        }
+        else //player is above ground level, assumes that player is inside building
+        {
+            //if player is in hallway
+            // 30% chance shadow is in hallway
+            // 85% chance shadow is behind
+            // 15% shadow is front
+            // 70% chance shadow is in classroom
+            //if player is in classroom
+            //
+        }
 
-        //need to add control statement for checking if player is on ground
     }
+
+    public
 
     bool IsPlayerGrounded()
     {
-        if ( player.transform.position.y <= 1.83 ) // is on ground
+        if (player.transform.position.y <= 1.83) // is on ground
         {
             return true;
         }
