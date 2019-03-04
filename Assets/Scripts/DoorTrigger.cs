505df554e81f816c6b2bbcaeadbf22f0f96d16d9
Albdog <joaquinjacinto11@gmail.com>
2019-03-04 21:24:08 +0800

    DoorTrigger - cleared useless code

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -16,10 +16,6 @@ public class DoorTrigger : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if(playerEntered && (RightHandInput.doorActivate || LeftHandInput.doorActivate)) {
-            float minPosX = gameObject.transform.position.x + 5f;
-            float maxPosX = gameObject.transform.position.x - 5f;
-            float minPosY = gameObject.transform.position.y + 5f;
-            float maxPosY = gameObject.transform.position.y + 0.1f;
             bool isOpen = doorAnimator.GetBool("isOpen");
 
             doorAnimator.SetBool("isOpen", !isOpen);
