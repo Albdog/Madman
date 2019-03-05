f2b52e412a0010a1738221a5461a009cca4b7953
Albdog <joaquinjacinto11@gmail.com>
2019-03-06 01:45:59 +0800

    PhoneManager - added logic for changing text

---
+++ Assets/Scripts/PhoneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhoneManager : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+        text.text =
+            "Outside - 1/2\n" +
+            "Bldg 1 - 0/3\n" +
+            "Bldg 2 - 2/2\n" +
+            "Bldg 4 - 1/2\n";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
