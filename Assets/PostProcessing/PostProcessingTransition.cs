0117666151cf7191f7dccc69b1eee10fb8034537
kurtdeleon <kurt.deleon@obf.ateneo.edu>
2019-03-28 00:45:24 +0800

    Added working transition script
    No smoothing function yet!

--- Assets/PostProcessing/PostProcessingTransition.cs
+++ Assets/PostProcessing/PostProcessingTransition.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.PostProcessing;
 
 public class PostProcessingTransition : MonoBehaviour
@@ -9,68 +7,89 @@ public class PostProcessingTransition : MonoBehaviour
     private GameObject player;
     private GameObject shadow;
 
-    private BloomModel.Settings bloom;
     private ChromaticAberrationModel.Settings chrome;
     private GrainModel.Settings grain;
     private MotionBlurModel.Settings blur;
+    private ColorGradingModel.Settings color;
 
-    const float MAX_DISTANCE = 18f;
+    const float MIN_DISTANCE = 18f;
+    //const float MAX_TRANSITION_TIME = 2.0f;
+    //private float timer;
+    private bool isNearToShadow;
 
-    private static class Ranges
+    private static class PPVRanges
     {
-        private static float[] bloomRadius = { 4f, 6.5f };
-        private static float[] bloomIntensity = { 1f, 15f };
-        private static float[] chromeIntensity = { 0f, 1f };
-        private static float[] grainIntensity = { 0f, 0.8f };
-        private static float[] grainSize = { 1f, 1.75f };
-        private static float[] motionAngle = { 1f, 360f };
-        private static float[] frameBlending = { 0f, 1f };
-        private static float[] playerShadowDistance = { 18f, 0.5f };
+        public static Vector2 colorRedMixer = new Vector2(1f, 2f);
+        public static Vector2 colorGreenMixer = new Vector2(0f, 1f);
+        public static Vector2 colorSaturation = new Vector2(1f, 0.4f);
+        public static Vector2 chromeIntensity = new Vector2(0f, 1f);
+        public static Vector2 grainIntensity = new Vector2(0.1f, 0.8f);
+        public static Vector2 grainSize = new Vector2(1f, 1.75f);
+        public static Vector2 motionAngle = new Vector2(1f, 360f);
+        public static Vector2 frameBlending = new Vector2(0f, 1f);
     }
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         shadow = GameObject.FindGameObjectWithTag("Shadow");
-        bloom = profile.bloom.settings;
-        chrome = profile.chromaticAberration.settings;
-        grain = profile.grain.settings;
-        blur = profile.motionBlur.settings;
-    }
-
-    private float GetDistanceBetween(Vector3 playerPos, Vector3 shadowPos)
-    {
-        return (playerPos - shadowPos).sqrMagnitude;
+        GetNewReferences();
+        //isNearToShadow = false;
+        //timer = 0;
     }
 
     void Update()
     {
-        float distance = GetDistanceBetween(player.transform.position, shadow.transform.position);
-        if ( distance > MAX_DISTANCE)
-        {
-            float lerp = distance / MAX_DISTANCE;
-        }
-        else
-        {
-
-        }
+        float distance = Vector3.Distance(player.transform.position, shadow.transform.position);
         
-        /*
-        ChromaticAberrationModel.Settings chrome = profile.chromaticAberration.settings;
-        if (Player.GetComponent<PlayerMove>().isSprinting == true)
+        float lerp = 0; //sets lerp to 0 (case when shadow is out of affected bounds)
+        if ( distance < MIN_DISTANCE) //checks if player is close to shadow (case when shadow is in bounds)
         {
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, runningFov, Time.deltaTime * runChangeSpeed);
-            chrome.intensity = Mathf.Lerp(chrome.intensity, 1, Time.deltaTime * Routes.range);
-            profile.chromaticAberration.settings = chrome;
-
-
+            lerp = 1 - (distance / MIN_DISTANCE);
+            //Debug.Log(lerp);
         }
-        else if (Player.GetComponent<PlayerMove>().isSprinting == false && GetComponent<Camera>().fieldOfView != 60f)
-        {
-            GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, 60f, Time.deltaTime * walkChangeSpeed);
-            chrome.intensity = Mathf.Lerp(chrome.intensity, 0, Time.deltaTime * runChangeSpeed);
-            profile.chromaticAberration.settings = chrome;
-        }
-        */
+
+        GetNewReferences(); //gets the current value from main profile
+        LerpAllValues(lerp); //accepts current lerp value and changes vision accordingly
+        UpdateNewReferences(); //sets the values in main profile to new settings
+    }
+
+    //TODO smooth function?
+    private void LerpAllValues(float lerp) 
+    {
+        chrome.intensity = Mathf.Lerp(PPVRanges.chromeIntensity.x, PPVRanges.chromeIntensity.y, lerp);
+        color.channelMixer.red = new Vector3(
+            Mathf.Lerp(PPVRanges.colorRedMixer.x, PPVRanges.colorRedMixer.y, lerp),
+            Mathf.Lerp(PPVRanges.colorGreenMixer.x, PPVRanges.colorGreenMixer.y, lerp),
+            color.channelMixer.red.z
+            );
+        color.basic.saturation = Mathf.Lerp(PPVRanges.colorSaturation.x, PPVRanges.colorSaturation.y, lerp);
+        grain.intensity = Mathf.Lerp(PPVRanges.grainIntensity.x, PPVRanges.grainIntensity.y, lerp);
+        grain.size = Mathf.Lerp(PPVRanges.grainSize.x, PPVRanges.grainSize.y, lerp);
+        blur.shutterAngle = Mathf.Lerp(PPVRanges.motionAngle.x, PPVRanges.motionAngle.y, lerp);
+        blur.frameBlending = Mathf.Lerp(PPVRanges.frameBlending.x, PPVRanges.frameBlending.y, lerp);
+    }
+
+    private void GetNewReferences()
+    {
+        chrome = profile.chromaticAberration.settings;
+        color = profile.colorGrading.settings;
+        grain = profile.grain.settings;
+        blur = profile.motionBlur.settings;
+    }
+
+    private void UpdateNewReferences()
+    {
+        profile.chromaticAberration.settings = chrome;
+        profile.colorGrading.settings = color;
+        profile.grain.settings = grain;
+        profile.motionBlur.settings = blur;
+    }
+
+    private void OnApplicationQuit()
+    {
+        GetNewReferences();
+        LerpAllValues(0);
+        UpdateNewReferences();
     }
 }
