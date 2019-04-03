6a19c9a89bb715ab29f40a9fa8b6f825ee34f524
kurtdeleon <kurt.deleon@obf.ateneo.edu>
2019-04-04 00:08:24 +0800

    Added sound effects manager
    Also updated opening scene audio files, as well as other sound effects

---
+++ Assets/Scripts/SoundEffectsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectsManager : MonoBehaviour {
+
+    private AudioSource audioSource;
+    [SerializeField] AudioClip[] effects;
+    private Dictionary<string, AudioClip> effectsDict;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        effectsDict = new Dictionary<string, AudioClip>
+        {
+            { "door close", effects[0] },
+            { "door open", effects[1] },
+            { "fix chair", effects[2] },
+            { "window close", effects[3] },
+            { "breaker fix", effects[4] },
+            { "switch flashlight", effects[5] },
+            { "switch phone", effects[6] }
+        };
+    }
+
+    public void PlaySoundEffect(string title)
+    {
+        if (effectsDict.ContainsKey(title))
+        {
+            audioSource.PlayOneShot(effectsDict[title], 1.0f);
+        }
+    }
+}
